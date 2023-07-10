abdabd5a8aa6fc693bd9745281b0bd0597316e55
Erik Evert <erikevert419@gmail.com>
2023-07-10 07:57:02 -0400

    Add pagination with will_paginate gem

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
     if params[:actor].present?
       @movies = @movies.where("actor ilike ?", "%#{params[:actor]}%")
     end
-    
+
+    @movies = @movies.paginate(page: params[:page], per_page: 1)  # 10 movies per page
   end
 end
